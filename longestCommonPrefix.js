2dc3f29a62511d211d348656e4f11183c2ad42b0
wendyshi <wendyshijob@gmail.com>
2016-09-14 00:15:22 -0700

    longest prefix js

---
+++ longestCommonPrefix.js
@@ -0,0 +1,29 @@
+var longestCommongPrefix = function(strs) {
+	var len = strs.length;
+	var result = '';
+
+	if(len === 0) {
+		return result;
+	}
+
+
+	for(var i = 0; i<strs.length; i++) {
+		var curChar = strs[0][i];
+		for(var j = 1; j<len; j++) {
+			if(curChar !== strs[j][i]){
+				return result;
+			}
+
+		if(strs[j].length === i) {
+			return result;
+		}
+	}
+
+	result = result + curChar;
+
+	}
+
+
+	
+	return result;
+};
